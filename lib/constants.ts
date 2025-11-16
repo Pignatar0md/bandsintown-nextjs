f2c1c9d6f8ec2199a12b01fe78b4d20dc78c7412
Marcelo Pignataro <pignataromd@gmail.com>
2025-11-16 22:57:45 +0000

    initial commit

---
+++ lib/constants.ts
@@ -0,0 +1,20 @@
+import { EventCardProps } from "@/components/EventCard";
+
+export const events: EventCardProps[] = [
+	{
+		slug: "event-1",
+		title: "Hellfest",
+		image: "/images/event111.jpg",
+		date: "2025-06-01",
+		time: "19:00 hs",
+		location: "Clisson, France",
+	},
+	{
+		slug: "event-2",
+		title: "Rock in Rio",
+		image: "/images/event-2.jpg",
+		date: "2025-06-01",
+		time: "21:00 hs",
+		location: "Rio de Janeiro, Brazil",
+	},
+];
