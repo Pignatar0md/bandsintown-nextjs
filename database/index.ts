b52281fc0f0eff52a9eeb997858e4fe473bacc01
Marcelo Pignataro <pignataromd@gmail.com>
2025-11-23 17:49:37 +0000

    implemeting of DB model

--- database/index.ts
+++ database/index.ts
@@ -2,3 +2,6 @@ import EventModel from "./event.model";
 import BookingModel from "./booking.model";
 
 export { EventModel, BookingModel };
+
+export type { Event } from "./event.model";
+export type { Booking } from "./booking.model";
